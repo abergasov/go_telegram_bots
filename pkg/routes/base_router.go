4ce1723a233547978d7bb4724caba5a282e0a402
Alexander Bergasov <a.bergasov@city-mobil.ru>
2020-12-26 20:21:56 +0300

    controll draft

--- pkg/routes/base_router.go
+++ pkg/routes/base_router.go
@@ -75,16 +75,18 @@ func (a *AppRouter) HandleClient(w http.ResponseWriter, r *http.Request) {
 		logger.Error("can't convert SingleBot to ControllerBot", errors.New("can't find bot"))
 	}
 
-	select {
-	case cmd := <-bot.CommandChain:
-		b, _ := json.Marshal(cmd)
-		err = c.WriteMessage(websocket.TextMessage, b)
-		if err != nil {
-			logger.Error("Error write in socket", err)
+	for {
+		select {
+		case cmd := <-bot.CommandChain:
+			b, _ := json.Marshal(cmd)
+			err = c.WriteMessage(websocket.TextMessage, b)
+			if err != nil {
+				logger.Error("Error write in socket", err)
+			}
+			logger.Info("received message", zap.String("data", string(b)))
+		default:
+			logger.Info("no activity")
 		}
-		logger.Info("received message", zap.String("data", string(b)))
-	default:
-		logger.Info("no activity")
 	}
 	logger.Info("Finish write in connection")
 }
